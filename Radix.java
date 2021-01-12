06f3c890ba36eccefca419fbf287c7574b6d5bb9
Jonathan <>
2021-01-12 13:20:29 -0500

    coded length

--- Radix.java
+++ Radix.java
@@ -11,6 +11,21 @@ public class Radix{
       nums[i] = n % 10;
       n /= 10;
     }
-    return nums[col];
+    return Math.abs(nums[col]);
+  }
+
+  public static int length(int n){
+    int temp = n;
+    int digits = 0;
+    if(temp == 0){
+      return 1;
+    }
+    else{
+        while(temp != 0){
+        temp /= 10;
+        digits++;
+      }
+    }
+    return digits;
   }
 }
--- RadixTester.java
+++ RadixTester.java
@@ -1,5 +1,13 @@
 public class RadixTester{
   public static void main(String[] args) {
     System.out.println(Radix.nth(123, 1));
+    System.out.println(Radix.nth(-123,1));
+    System.out.println(Radix.nth(123, 2));
+    System.out.println(Radix.nth(123, 2));
+    System.out.println(Radix.length(0));
+    System.out.println(Radix.length(1));
+    System.out.println(Radix.length(15));
+    System.out.println(Radix.length(-10));
+    System.out.println(Radix.length(5112));
   }
 }
